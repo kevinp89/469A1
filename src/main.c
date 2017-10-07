a2b3a7f28b71a819751d2c2e43bd726c872ccaa0
root <root@localhost.localdomain>
2017-10-06 23:38:00 -0400

    added the argc error check

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include "tsc.h"
 
@@ -17,18 +18,27 @@ u_int64_t inactive_periods(int num, u_int64_t threshold, u_int64_t *samples)
 		current_reading = get_counter();
 
 		if((current_reading - prev_reading) >= threshold){
-			samples[idx] = current_reading;
+			samples[2*idx] = prev_reading;
+			samples[2*idx + 1] = current_reading;
 			idx++;
 		}
 	}
-
 
 	return initial_reading;
 }
 
 int main(int argc, char *argv[])
 {
-    /* u_int64_t samples[100]; */
-    inactive_periods(0,0, 0);
-    return 0;
+
+        if(argc != 2){
+		fprintf("Usage: ./main <number of inactive periods>");
+	   	exit();
+	}
+		
+	int num_inactive = atoi(argv[1]);
+
+	struct timespec clock_speed;	
+    	/* u_int64_t samples[100]; */
+   	inactive_periods(num_inactive,0, 0);
+   	return 0;
 }
