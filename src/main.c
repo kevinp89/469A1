b4cf181dc54a53813a8a138fb48d9117b1a24c2d
root <root@localhost.localdomain>
2017-10-07 00:28:53 -0400

    sampling sleeptime loop added

--- src/main.c
+++ src/main.c
@@ -37,7 +37,21 @@ int main(int argc, char *argv[])
 		
 	int num_inactive = atoi(argv[1]);
 
-	struct timespec clock_speed;	
+	struct timespec sleep_timer;	
+	sleep_time.tv_sec = 0;
+	sleep_time.tv_nsec = 1000000L; // 1 milisecond
+	
+	//int count_miliseconds = 0;
+	int samples[4];	
+	int i;
+	for(i=0, i<3; i++){
+		start_counter();
+		if(nano_sleep(&sleep_timer, NULL) == 0){
+			samples[i] = get_counter();
+		}
+	}
+	
+	
     	/* u_int64_t samples[100]; */
    	inactive_periods(num_inactive,0, 0);
    	return 0;
