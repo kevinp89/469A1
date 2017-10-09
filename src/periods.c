a25ab227cc63cb142b240e7a563f3d9abf47ffd1
sach1t <sach0010t@gmail.com>
2017-10-09 11:25:43 -0400

    Add function description

--- src/periods.c
+++ src/periods.c
@@ -1,6 +1,12 @@
 #include <sys/types.h>
 #include "tsc.h"
 
+/* Records num inactive periods in the samples array, where an inactive period
+ * is determined by whether it is longer than threshold cycles.
+ *
+ * Note, samples is an array of size 2*num where samples[2*i] = start of period,
+ * and samples[2*i+1] = end of period.
+ */
 u_int64_t inactive_periods(int num, u_int64_t threshold, u_int64_t *samples)
 {
 	u_int64_t first, prev, curr;
