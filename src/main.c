2f29785c3c78cef356d7a94abd6a19503e97a39d
sach1t <sach0010t@gmail.com>
2017-10-09 13:09:52 -0400

    Fix indenting

--- src/main.c
+++ src/main.c
@@ -20,35 +20,35 @@ double calc_cpu_freq(int num_samples, long ns_timeout)
 		start_counter();
 		if(nanosleep(&sleep_timer, NULL) == 0) {
 			samples[i] = get_counter();
-        } else {
-            perror("Error nano-sleeping");
-            exit(2);
-        }
+		} else {
+			perror("Error nano-sleeping");
+			exit(2);
+		}
 	}
 
 	double min = samples[0];
-    for (int i = 0; i < num_samples; i++) {
+	for (int i = 0; i < num_samples; i++) {
 		printf("sample[%d]: %f\n", i, samples[i]);
-        min = min < samples[i]? min : samples[i];
-    }
-    printf("min: %f\n", min);
+		min = min < samples[i]? min : samples[i];
+	}
+	printf("min: %f\n", min);
 
 	double cpu_hz = (min / sleep_timer.tv_nsec) * 1e9;
 	printf("cpu: %fMHz\n", cpu_hz / 1e6);
-    return cpu_hz;
+	return cpu_hz;
 }
 
 int main(int argc, char *argv[])
 {
-    if(argc != 2){
+	if(argc != 2){
 		fprintf(stderr, "Usage: %s <number of inactive periods>\n", argv[0]);
-	   	return 1;
+		return 1;
 	}
 
 	/* int num_inactive = atoi(argv[1]); */
 
-    double hz = calc_cpu_freq(6, 10000000L /* 10 ms */);
+	double hz = calc_cpu_freq(6, 10000000L /* 10 ms */);
 
-	
-   	return 0;
+
+	return 0;
 }
